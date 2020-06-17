5ec6de3639b4db4ba7e64ad2e810601e1b728314
Calenaur <timhoekstra2015@gmail.com>
2020-06-17 14:11:06 +0200

    Add DebugActivity

---
+++ app/src/main/java/com/calenaur/pandemic/activity/DebugActivity.java
@@ -0,0 +1,19 @@
+package com.calenaur.pandemic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("username", "");
+        intent.putExtra("password", "");
+        startActivity(intent);
+        finish();
+    }
+}
