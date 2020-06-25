0b7a06f46c24854220776bffe6b7b2d20a80ef9f
Arjan <arjan@koens.biz>
2020-06-25 14:43:14 +0200

    Intermedite progress
    need to push to rebase for medicine store.

--- app/src/main/java/com/calenaur/pandemic/fragment/MedicineFragment.java
+++ app/src/main/java/com/calenaur/pandemic/fragment/MedicineFragment.java
@@ -15,12 +15,17 @@ import android.widget.LinearLayout;
 
 import com.calenaur.pandemic.R;
 import com.calenaur.pandemic.SharedGameDataViewModel;
+import com.calenaur.pandemic.api.model.medication.Medication;
 
 public class MedicineFragment extends Fragment {
 
     private SharedGameDataViewModel balanceViewModel;
 
+    private LinearLayout medicineFragment;
     private LinearLayout container;
+
+    private static final Medication[] medications = {new Medication(1,"paper", 2), new Medication(2,"pil", 5)};
+
 
     @Nullable
     @Override
@@ -30,10 +35,17 @@ public class MedicineFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        medicineFragment = view.findViewById(R.id.medicineFragment);
         container = new LinearLayout(getContext());
 
+        for(Medication medication: medications){
+            
 
-        container.addView();
+            container.addView();
+        }
+
+
+        medicineFragment.addView(container);
         super.onViewCreated(view, savedInstanceState);
     }
 
